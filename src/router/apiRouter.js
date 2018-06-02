4825f265dc05f4b72ed25ddb49b2e14b51a5ca0b
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-02 14:23:04 +0100

    optional route param

--- src/router/apiRouter.js
+++ src/router/apiRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { fetchMovies } = require('./controller/apiController');
 
-router.get('/movies/:category', (req, res) => {
-  res.json({ message: 'hello, world 1' });
-});
+router.get('/movies/:category?/', fetchMovies);
 
 router.get('/movie/:id/:title?/', (req, res) => {
   res.json({ message: 'hello, world' });
