45d3cce40c751fddc42ce6052e0d7cba0a1490d3
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-02 07:19:29 +0100

    I swear I didn't freeze. I just configured the server.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// do not freeze!
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '../public/')));
+
+const PORT = process.env.PORT || 2222;
+
+app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT}. Visit http://localhost:${PORT}`)
+);
