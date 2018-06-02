d365ddd290509f492719f2667a5169663cefefa3
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-02 12:12:02 +0100

    use app router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const appRoutes = require('./router');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('view engine', 'pug');
+app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, '../public/')));
+
+appRoutes(app);
 
 const PORT = process.env.PORT || 2222;
 
