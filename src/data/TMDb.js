1c95ff34ebdcf3ab4838049699baded6e75d38c7
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-02 14:22:37 +0100

    create TMDb config file

---
+++ src/data/TMDb.js
@@ -0,0 +1,92 @@
+const image = {
+  config: {
+    base_url: 'https://image.tmdb.org/t/p',
+    backdrop_size: {
+      original: '/original',
+      high: '/w1280',
+      medium: '/w780',
+      low: '/w300'
+    },
+    poster_size: {
+      original: '/original',
+      high: '/w500',
+      medium: '/w342',
+      low: '/w185',
+      xlow: '/w154',
+      xxlow: '/w92'
+    }
+  }
+};
+
+const configPoster = function(size = 'xxlow') {
+  let poster_size = size;
+  const base_url = image.config.base_url;
+  const { high, medium, low, xlow, xxlow, original } = image.config.poster_size;
+
+  switch (size) {
+    case 'high':
+      poster_size = high;
+      break;
+    case 'medium':
+      poster_size = medium;
+      break;
+    case 'low':
+      poster_size = low;
+      break;
+    case 'xlow':
+      poster_size = xlow;
+      break;
+    case 'xxlow':
+      poster_size = xxlow;
+      break;
+    default:
+      poster_size = original;
+      break;
+  }
+
+  return `${base_url}${poster_size}`;
+};
+
+const configBackdrop = function(size = 'low') {
+  let backdrop_size = size;
+  const base_url = image.config.base_url;
+  const { high, medium, low, original } = image.config.backdrop_size;
+
+  switch (size) {
+    case 'high':
+      backdrop_size = high;
+      break;
+    case 'medium':
+      backdrop_size = medium;
+      break;
+    case 'low':
+      backdrop_size = low;
+      break;
+    default:
+      backdrop_size = original;
+      break;
+  }
+
+  return `${base_url}${backdrop_size}`;
+};
+
+module.exports = {
+  base_url: 'https://api.themoviedb.org/3',
+  movie: {
+    category: {
+      popular: '/movie/popular',
+      top_rated: '/movie/top_rated',
+      now_playing: '/movie/now_playing'
+    },
+    poster: {
+      build: (poster_path, size = 'xxlow') => {
+        return configPoster(size) + poster_path;
+      }
+    },
+    backdrop: {
+      build: (backdrop_path, size = 'low') => {
+        return configBackdrop(size) + backdrop_path;
+      }
+    }
+  }
+};
