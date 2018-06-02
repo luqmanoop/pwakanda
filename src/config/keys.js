a53ec51d6e678dec3583b971be5d6188e16534d7
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-02 14:24:04 +0100

    removed unnecessary extension

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,3 +1,2 @@
-if (process.env.NODE_ENV === 'production')
-  module.exports = require('./prod.js');
-else module.exports = require('./dev.js');
+if (process.env.NODE_ENV === 'production') module.exports = require('./prod');
+else module.exports = require('./dev');
