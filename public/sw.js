a028a83180e5739b8a8b921f155e06253bff816c
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-03 12:55:35 +0100

    precache app shell

--- public/sw.js
+++ public/sw.js
@@ -14,3 +14,13 @@ const urlsToCache = [
   'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
 ];
+/**
+ * precache App Shell
+ */
+function precache() {
+  return caches.open(movieStaticCache).then(cache => {
+    console.log('[ServiceWorker] Caching app shell');
+    return cache.addAll(urlsToCache);
+  });
+}
+
