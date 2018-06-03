784d3433b717fd1c380c9ee2fe24047c3c3a1268
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-03 12:49:34 +0100

    capture app shell files in array

--- public/sw.js
+++ public/sw.js
@@ -3,3 +3,13 @@ const movieDataCache = 'pwakanda-data-v1';
 const movieImageCache = 'pwakanda-images-v1';
 
 const wakandaCaches = [appShell, movieDataCache, movieImageCache];
+const urlsToCache = [
+  '/',
+  '/css/style.css',
+  '/css/detail.css',
+  '/js/app.bundle.js',
+  '/js/movie.bundle.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+  'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
+];
