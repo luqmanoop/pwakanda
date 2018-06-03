04c014e28265981c1cf3f2a27c6fd5e1edc96acc
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-03 12:47:30 +0100

    create cache names for different data to be cached

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const appShell = 'pwakanda-static-v1';
+const movieDataCache = 'pwakanda-data-v1';
+const movieImageCache = 'pwakanda-images-v1';
+
+const wakandaCaches = [appShell, movieDataCache, movieImageCache];
