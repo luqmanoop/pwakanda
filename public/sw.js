94e50259dccc82ca740dd4eeded9f35a00730393
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-04 12:42:40 +0100

    streamline app shell

--- public/sw.js
+++ public/sw.js
@@ -7,9 +7,7 @@ const wakandaCaches = [movieStaticCache, movieDataCache, movieImageCache];
 const urlsToCache = [
   '/',
   '/css/style.css',
-  '/css/detail.css',
   '/js/app.bundle.js',
-  '/js/movie.bundle.js',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
   'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
