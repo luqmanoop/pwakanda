d0de3fc0a8f9bed0be0bac529e5fb959f82f6299
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-05 19:12:56 +0100

    create date formatter

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,3 +8,12 @@ exports.slugify = text => {
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, ''); // Trim - from end of text
 };
+
+exports.formatDate = date => {
+  const release_date = new Date(date)
+    .toDateString()
+    .split(' ')
+    .splice(1);
+
+  return `${release_date[0]} ${release_date[1]}, ${release_date[2]}`;
+};
