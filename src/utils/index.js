627dd1cf564038eba359f9dedc384dd442863e16
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-06 13:39:39 +0100

    created more util func

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,3 +17,22 @@ exports.formatDate = date => {
 
   return `${release_date[0]} ${release_date[1]}, ${release_date[2]}`;
 };
+
+exports.renderMovieRatings = rating => {
+  const TOTAL_RATING = 5;
+  const RATED = caculateRatings(rating);
+
+  let ratings = '';
+  for (let i = 1; i <= TOTAL_RATING; i++) {
+    if (i <= RATED) {
+      ratings += '<span class="rating glyphicon glyphicon-star"></span>';
+    } else {
+      ratings += '<span class="rating glyphicon glyphicon-star-empty"></span>';
+    }
+  }
+  return ratings;
+};
+
+const caculateRatings = rating => {
+  return Number.parseFloat((rating / 10) * 5).toFixed();
+};
