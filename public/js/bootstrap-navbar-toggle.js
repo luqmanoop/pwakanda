81f0e5ee81016a00d82f0f4c17745a21aec13d85
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-07 10:55:37 +0100

    removed unused func

--- public/js/bootstrap-navbar-toggle.js
+++ public/js/bootstrap-navbar-toggle.js
@@ -7,20 +7,9 @@ function toggleMenu() {
   collapse.classList.toggle('in');
 }
 
-// Close all dropdown menus
-function closeMenus() {
-  dropdowns.forEach(dropdown => {
-    dropdown
-      .querySelectorAll('.dropdown-toggle')[0]
-      .classList.remove('dropdown-open');
-    dropdown.classList.remove('open');
-  });
-}
-
 // Close dropdowns when screen becomes big enough to switch to open by hover
 function closeMenusOnResize() {
   if (document.body.clientWidth >= 768) {
-    closeMenus();
     collapse.classList.add('collapse');
     collapse.classList.remove('in');
   }
