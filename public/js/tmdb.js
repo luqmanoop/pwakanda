05737f00ec5aac9ed7f7a3533a7e2be281a0a713
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-09 05:18:51 +0100

    support fetch options

--- public/js/tmdb.js
+++ public/js/tmdb.js
@@ -1,7 +1,7 @@
 require('whatwg-fetch');
 
-const fetchMovies = url => {
-  return fetch(url)
+const fetchMovies = (url, opts) => {
+  return fetch(url, opts)
     .then(response => response.json())
     .then(movies => movies)
     .catch(err => null);
