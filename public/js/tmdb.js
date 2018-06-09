a4662396182c8b5177a6ef864930f16cd77d9db8
Olushi Luqman O <olushilukmon03@gmail.com>
2018-06-09 06:54:26 +0100

    switched to to es6 module export

--- public/js/tmdb.js
+++ public/js/tmdb.js
@@ -1,10 +1,8 @@
 require('whatwg-fetch');
 
-const fetchMovies = (url, opts) => {
+export const fetchMovies = (url, opts) => {
   return fetch(url, opts)
     .then(response => response.json())
     .then(movies => movies)
     .catch(err => null);
 };
-
-module.exports.fetchMovies = fetchMovies;
